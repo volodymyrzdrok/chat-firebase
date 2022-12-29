8cd0f92953ffee7202b13ef507cc312f2106e432
Volodymyr Zdrok <volodymyrzdrok44@gmail.com>
2022-12-29 12:22:57 +0200

    fwefwef

--- src/firebase-config/index.js
+++ src/firebase-config/index.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDymg4vks7hJSwct7cLu25taytiyIVDlbM",
-  authDomain: "to-do-list40.firebaseapp.com",
-  projectId: "to-do-list40",
-  storageBucket: "to-do-list40.appspot.com",
+  apiKey: "AIzaSyAby_JieMWWl3FmTPjjvi5nBSc-VAp6_8k",
+  authDomain: "practic-js-d085d.firebaseapp.com",
   databaseURL:
-    "https://to-do-list40-default-rtdb.europe-west1.firebasedatabase.app",
-  messagingSenderId: "451896663745",
-  appId: "1:451896663745:web:6c7c29232913b8b317be40",
+    "https://practic-js-d085d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "practic-js-d085d",
+  storageBucket: "practic-js-d085d.appspot.com",
+  messagingSenderId: "622586783219",
+  appId: "1:622586783219:web:11e59dcfc12e8d4a339e86",
 };
